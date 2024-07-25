d120ef2d090a35d98689ba9c20d9310e425dcfb3
sort9 <143142285+sort9@users.noreply.github.com>
2024-07-25 10:51:25 -0400

    Update modulo.cpp
    multiple issues in the code

--- modulo.cpp
+++ modulo.cpp
@@ -2,12 +2,29 @@
 using namespace std;
 
 int remainderX;
-int num1, num2, remains;
+float num1, num2, remains;
 
-int modulate(int n1, int n2);
-void factorCheck(int n1, int n2);
+int modulate(float n1, float n2);
+void factorCheck(float n1, float n2);
+void positive_dividend_op();
+void negative_dividend_op();
 
 int main(void) {
+    int choice;
+    cout << "1. Positive dividend modulo operations i.e. 16 mod 8" << endl;
+    cout << "2. Negative dividend modulo operations i.e. -15 mod 9" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            positive_dividend_op();
+        case 2:
+            negative_dividend_op();
+    }
+    return 0;
+}
+
+void positive_dividend_op() {
     cout << "Number before mod: ";
     cin >> num1;
     cout << "Number after mod: ";
@@ -16,16 +33,25 @@ int main(void) {
     cout << "Operation performed: " << num1 << " mod " << num2 << "\n";
     cout << "Answer: " << remains;
     factorCheck(num1, num2);
-    return 0;
 }
 
-int modulate(int n1, int n2) {
+int modulate(float n1, float n2) {
     remainderX = n1 % n2;
     return remainderX;
 }
 
-void factorCheck(int n1, int n2) {
+void factorCheck(float n1, float n2) {
     if (remainderX == 0) {
         cout << "\n" << n1 << " is a factor of " << n2;
     }
+}
+
+void negative_dividend_op() {
+    cout << "Number before mod: ";
+    cin >> num1;
+    cout << "Number after mod: ";
+    cin >> num2;
+    remainderX = (2 * num2) - num1;
+    cout << "Operation performed: -" << num1 << " mod " << num2 << "\n";
+    cout << "Answer: " << remainderX;
 }
